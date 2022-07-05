f9dc934dcc58850331ea258a9fb45eadabedc64e
RezaRizkiA <rezariski.akbar@gmail.com>
2022-07-05 15:41:07 +0700

    membuat table manual tanpa database

---
+++ pertemuan1/pertemuan1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+
+<body>
+    <h3>Data Mahasiswa</h3>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Gambar</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td><img src="img/kambing.jpg"></td>
+            <td>123456789</td>
+            <td>Kambing Qurban</td>
+            <td>kambing@gmail.com</td>
+            <td>Fakultas Kedokteran</td>
+        </tr>
+    </table>
+</body>
+
+</html>
